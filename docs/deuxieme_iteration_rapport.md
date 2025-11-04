0cba5a5a6e2970ff45345e48f100f6091b3767a5
qiyunge <157900653@qq.com>
2025-11-03 22:12:05 -0500

    deuxieme  iteration

---
+++ docs/deuxieme_iteration_rapport.md
@@ -0,0 +1,26 @@
+# rapport de deuxieme iteration
+
+Github: [https://github.com/gqysmart/cppcourse-bankATM]( https://github.com/gqysmart/cppcourse-bankATM)
+
+file: src/App.cpp
+
+**Fonctionnalités :**
+
+- Affichage clair du solde actuel
+- Menu avec options numérotées
+- Retour au menu principal après consultation
+
+*Exemple d’utilisation :*
+
+Collège LaSalle
+
+Menu principal :
+
+1. Consulter solde
+2. Déposer de l’argent
+3. Retirer de l’argent
+4. Quitter
+
+Choix : 1
+
+Votre solde actuel est de : 1523.75 €
--- src/App.cpp
+++ src/App.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
+int printMenu();
 int main()
 {
     const string password = "2580";
     string pin = "";
+    double solde_db = 1523.75;
     for (int i = 1; i <= 3; i++)
     {
         pin = "";
@@ -17,8 +19,36 @@ int main()
         else
         {
             cout << "Accès autorisé." << endl;
-            break;
+            printMenu();
+            int choix;
+            cin >> choix;
+            while (choix != 4)
+            {
+                switch (choix)
+                {
+                case 1:
+                    cout << "Votre solde actuel est de " << solde_db << "€" << endl;
+                    break;
+
+                default:
+                    break;
+                }
+                printMenu();
+                cin >> choix;
+            }
         }
     }
+
+    return 0;
+}
+
+int printMenu()
+{
+    cout << "menu principal" << endl;
+    cout << 1 << " Consulter solde" << endl;
+    cout << 2 << " Déposer de l’argent" << endl;
+    cout << 3 << " Retirer de l’argent" << endl;
+    cout << 4 << " Quitter" << endl;
+    cout << "donnez votre Choix " << endl;
     return 0;
 }
